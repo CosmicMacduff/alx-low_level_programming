ecad46b11579b86c172108a6b97dd5edf06cb0dd
cosmicmacduff <emetkalumacduff@gmail.com>
2023-02-20 12:34:09 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+		}	putchar(' ');
+	}
+	return (0);
+}
