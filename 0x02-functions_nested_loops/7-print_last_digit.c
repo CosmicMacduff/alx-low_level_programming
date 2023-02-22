f899b1d4861c38c2db70c61466a030f09107f114
cosmicmacduff <emetkalumacduff@gmail.com>
2023-02-22 15:31:17 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints last digit
+ *
+ * @i: function parameter
+ *
+ * Return: k
+ */
+
+
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}
