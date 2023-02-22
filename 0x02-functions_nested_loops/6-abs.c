d209ea5ca2f319f3cf467c96619e4348f97fd5a2
cosmicmacduff <emetkalumacduff@gmail.com>
2023-02-22 15:08:13 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - checks for absolute value
+ *
+ * @a: parameter to be checked
+ *
+ * Return: always a
+ */
+
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
