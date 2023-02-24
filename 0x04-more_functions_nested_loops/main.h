ed4823edcf0861764d74db93de1a3bc1146f0418
cosmicmacduff <emetkalumacduff@gmail.com>
2023-02-24 15:04:49 -0800

    Task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _putchar(int c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -12,6 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif
-
