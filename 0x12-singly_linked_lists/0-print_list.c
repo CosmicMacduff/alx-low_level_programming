be99919863121cadfe06d4706e76022b58820fc9
CosmicMacduff <emetukalumacduff@gmail.com>
2023-03-30 17:12:25 -0700

    TASK 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,15 @@
  */
 size_t print_list(const list_t *h)
 {
- size_t s = 0;
-
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- s++;
- }
-
- return (s);
+size_t s = 0;
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+s++;
+}
+return (s);
 }
